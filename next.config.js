42c233d63541d762078c910b671886618c5dd75a
Mayron <mayronfernandes01@gmail.com>
2023-03-06 12:31:48 -0300

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+}
 
 module.exports = nextConfig
